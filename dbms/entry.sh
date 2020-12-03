7b62081e64e4a99aea5877fff3adb0222a4c19a8
Yifei Li <liyifei19@mails.tsinghua.edu.cn>
2020-12-04 02:03:01 +0800

    Mongo rebase to pure ubuntu.

--- dbms/entry.sh
+++ dbms/entry.sh
@@ -5,7 +5,8 @@ echo "Starting MongoDB..."
 # systemctl daemon-reload
 # systemctl start mongod
 # systemctl status mongod
-mlaunch init --replicaset --sharded 3 --nodes 3 --config 3 --mongos 1 --dir $DATA_DIR --hostname $CONTAINER_NAME --bind_ip_all
+mlaunch init --replicaset --sharded beijing hongkong --nodes 3 \
+    --config 3 --mongos 1 --dir $DATA_DIR --hostname $CONTAINER_NAME --bind_ip_all
 echo "MongoDB started."
 
 # import JSON files to MongoDB
