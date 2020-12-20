65fe2aaef40eb25e56c2e2969b2006e9a59ebb80
Yifei Li <liyifei19@mails.tsinghua.edu.cn>
2020-12-20 16:11:02 +0800

    Allow mongo reuse db data.

--- dbms/entry.sh
+++ dbms/entry.sh
@@ -2,16 +2,17 @@
 
 # start MongoDB service
 echo "Starting MongoDB..."
-# systemctl daemon-reload
-# systemctl start mongod
-# systemctl status mongod
-mlaunch init --replicaset --sharded beijing hongkong --nodes 3 \
-    --config 3 --mongos 1 --dir $DATA_DIR --hostname $CONTAINER_NAME --bind_ip_all
+if [ ! -d $DATA_DIR ]; then
+    mlaunch init --replicaset --sharded beijing hongkong --nodes 3 \
+        --config 3 --mongos 1 --dir $DATA_DIR --hostname $CONTAINER_NAME --bind_ip_all
+    # shard setup
+    echo "Setting up shard..."
+    mongo ./setup.js
+    echo "Finished!"
+else
+    mlaunch start --dir $DATA_DIR
+fi
 echo "MongoDB started."
 
-# shard setup
-echo "Setting up shard..."
-mongo ./setup.js
-echo "Finished!"
-
+# keep running
 tail -f /dev/null
